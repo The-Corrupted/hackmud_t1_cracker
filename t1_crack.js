51fcceda74a8b00a22c101be0051f5667c874815
Donovan <intothedark.me@gmail.com>
2020-03-09 20:10:01 -0700

    Update t1_crack.js
    hide user

--- t1_crack.js
+++ t1_crack.js
@@ -14,37 +14,37 @@ function(c, a) { // target:#s.t1npc.corp
 		m = m[m.length-1];
 		switch(m.toLowerCase()) {
 			case 'ez_21':
-				var j = #fs.thecorrupted.ez_lock({target: t, ez_lock: 'ez_21', lc: lc});
+				var j = #fs.user.ez_lock({target: t, ez_lock: 'ez_21', lc: lc});
 				lc = j.msg[1];
 				r = j.msg[0];
 				break;
 			case 'ez_40':
-				var j = #fs.thecorrupted.ez_lock({target: t, ez_lock: 'ez_40', lc: lc});
+				var j = #fs.user.ez_lock({target: t, ez_lock: 'ez_40', lc: lc});
 				lc = j.msg[1];
 				r = j.msg[0];
 				break;
 			case 'ez_35':
-				var j = #fs.thecorrupted.ez_lock({target: t, ez_lock: 'ez_35', lc: lc});
+				var j = #fs.user.ez_lock({target: t, ez_lock: 'ez_35', lc: lc});
 				lc = j.msg[1];
 				r = j.msg[0];
 				break;
 			case 'c001': 
-				var j = #fs.thecorrupted.c00_lock({target: t, c00_lock: 'c001', lc: lc});
+				var j = #fs.user.c00_lock({target: t, c00_lock: 'c001', lc: lc});
 				lc = j.msg[1];
 				r = j.msg[0];
 				break;
 			case 'c002':
-				var j = #fs.thecorrupted.c00_lock({target: t, c00_lock: 'c002', lc: lc});
+				var j = #fs.user.c00_lock({target: t, c00_lock: 'c002', lc: lc});
 				lc = j.msg[1];
 				r = j.msg[0];
 				break;
 			case 'c003':
-				var j = #fs.thecorrupted.c00_lock({target: t, c00_lock: 'c003', lc: lc});
+				var j = #fs.user.c00_lock({target: t, c00_lock: 'c003', lc: lc});
 				lc = j.msg[1];
 				r = j.msg[0];
 				break;
 			case 'l0cket':
-				var j = #fs.thecorrupted.ez_lock({target: t, ez_lock: 'l0cket', lc: lc});
+				var j = #fs.user.ez_lock({target: t, ez_lock: 'l0cket', lc: lc});
 				lc = j.msg[1];
 				r = j.msg[0];
 				break;
